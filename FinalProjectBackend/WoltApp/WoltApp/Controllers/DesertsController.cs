15803c89f22d22acb682cd11a5e65a0d8d08eb00
Norucova <you@example.com>
2022-02-26 19:08:16 +0400

    added CategoryController

---
+++ FinalProjectBackend/WoltApp/WoltApp/Controllers/DesertsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WoltDataAccess.DAL;
+using WoltEntity.Entities;
+
+namespace WoltApp.Controllers
+{
+    public class DesertsController : Controller
+    {
+        private AppDbContext _context { get; }
+
+        public DesertsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<RestaurantCategory> RestaurantCategories = await _context.RestaurantCategories.Include(c => c.Restaurant)
+                                                                          .Where(c => c.CategoryId == 6)
+                                                                          .Include(c => c.Category).ToListAsync();
+            return View(RestaurantCategories);
+        }
+    }
+}
---
+++ FinalProjectBackend/WoltApp/WoltApp/Controllers/SandwichController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WoltDataAccess.DAL;
+using WoltEntity.Entities;
+
+namespace WoltApp.Controllers
+{
+    public class SandwichController : Controller
+    {
+        private AppDbContext _context { get; }
+
+        public SandwichController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<RestaurantCategory> RestaurantCategories = await _context.RestaurantCategories.Include(c => c.Restaurant)
+                                                                          .Where(c => c.CategoryId == 11)
+                                                                          .Include(c => c.Category).ToListAsync();
+            return View(RestaurantCategories);
+        }
+    }
+}

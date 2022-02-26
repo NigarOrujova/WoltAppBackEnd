84d1d34485db178f3ddc5716501ff70e2f21ff50
Norucova <you@example.com>
2022-02-26 14:06:56 +0400

    added BurgerController.cs File

---
+++ FinalProjectBackend/WoltApp/WoltBusiness/DTOs/CategoryDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WoltEntity.Entities;
+
+namespace WoltBusiness.DTOs
+{
+    public class CategoryDTO
+    {
+        public List<Restaurant> Restaurants { get; set; }
+        public List<RestaurantProduct> RestaurantProducts { get; set; }
+        public List<RestaurantCategory> RestaurantCategories { get; set; }
+    }
+}

13e89cc87f4753503412075fd6aecf4f932be02f
Norucova <you@example.com>
2022-03-05 17:07:45 +0400

    Modified homepage

--- FinalProjectBackend/WoltApp/WoltApp/wwwroot/assets/js/script.js
+++ FinalProjectBackend/WoltApp/WoltApp/wwwroot/assets/js/script.js
@@ -130,6 +130,29 @@ $('.slide-five').owlCarousel({
     }
 })
 
+$('.slide-six').owlCarousel({
+    margin: 10,
+    dots: false,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: true,
+            loop: false
+        }
+    }
+})
+
 const ScrollToTop = document.querySelector('.ScrollToTop');
 window.addEventListener('scroll', () => {
     if (window.pageYOffset > 100) {
